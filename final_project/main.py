c278ca528dd9b48ba25b176f114a9d0aec0563d3
ItayChabra <itaychabra@gmail.com>
2024-09-02 17:23:54 +0300

    17th Commit 2.9

--- final_project/main.py
+++ final_project/main.py
@@ -10,7 +10,6 @@ def main():
     else:
         # Run the interpreter in interactive mode
         interpreter = Interpreter()
-        interpreter.run_program('test_program.lambda')
         interpreter.repl()
 
 if __name__ == "__main__":
